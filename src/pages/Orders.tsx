6187f0dd09e4463b040ebf96fb19f13b78d5f075
bogdan demchenko <bogdandemchenko@gmail.com>
2021-12-26 11:35:43 +0200

    render orders

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,12 +1,67 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Layout from "../components/Layout";
+import {Order} from "../models/order";
+import axios from "axios";
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow
+} from "@material-ui/core";
 
 const Orders = () => {
-  return (
-      <Layout>
+    const [orders, setOrders] = useState<Order[]>([]);
+    useEffect(() => {
+        (
+            async () => {
+                const {data} = await axios.get(`orders`);
 
-      </Layout>
-  );
+                setOrders(data);
+            }
+        )()
+    }, []);
+
+    return (
+        <Layout>
+            {orders.map(order => {
+                return (
+                    <Accordion key={order.id}>
+                        <AccordionSummary>
+                            {order.name} ${order.total}
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>#</TableCell>
+                                        <TableCell>Product Title</TableCell>
+                                        <TableCell>Price</TableCell>
+                                        <TableCell>Quantity</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {order.order_items.map(item => {
+                                        return (
+                                            <TableRow key={item.id}>
+                                                <TableCell>{item.id}</TableCell>
+                                                <TableCell>{item.product_title}</TableCell>
+                                                <TableCell>{item.price}</TableCell>
+                                                <TableCell>{item.quantity}</TableCell>
+                                            </TableRow>
+                                        );
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </AccordionDetails>
+                    </Accordion>
+                );
+            })}
+        </Layout>
+    );
 };
 
 export default Orders;
